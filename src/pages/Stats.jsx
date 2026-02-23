ffbff4c462e4ec4219bcc098ca6316ea17cf7cc6
Alexey Titov <Al.chef.Titov@yandex.ru>
2026-02-23 17:31:23 +0300

    Добавили  страницу Stats с загрузчиком и функцией useLoaderData

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,3 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+
+export async function statsLoader() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/1');
+  if (!res.ok) throw new Response("Failed to load stats", { status: 500 });
+  return res.json();
+}
+
 export default function Stats() {
-  return <h2>Stats</h2>;
+  const user = useLoaderData();
+
+  return (
+    <div>
+      <h2>Stats</h2>
+      <p>User: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
 }
--- src/router.jsx
+++ src/router.jsx
@@ -11,6 +11,7 @@ import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import Stats from "./pages/Stats";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { statsLoader } from "./pages/Stats"
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,7 +23,7 @@ export const router = createBrowserRouter(
         <Route path="/dashboard" element={<Dashboard />}>
           <Route path="profile" element={<Profile />} />
           <Route path="settings" element={<Settings />} />
-          <Route path="stats" element={<Stats />} />
+          <Route path="stats" element={<Stats />} loader={statsLoader} />
         </Route>
       </Route>
     </>
